4240f18920e37f21f126e30061ee550c276ad84d
Matteo Collina <hello@matteocollina.com>
2025-09-28 14:08:08 +0200

    fix caching
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- caching/verify.js
+++ caching/verify.js
@@ -6,7 +6,7 @@ let requestIndex = 0
 const result = await autocannon({
   url: `http://127.0.0.1:3000`,
   connections: parseInt(process.env.CONNECTIONS || '100'),
-  pipelining: 10,
+  pipelining: 100,
   duration: parseInt(process.env.DURATION || '60'),
   requests: [
     {
